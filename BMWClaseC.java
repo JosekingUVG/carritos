60398ce4b11e14d291093276fbb69db4231b546d
JoseKingUVG <riv24376@uvg.edu.gt>
2024-11-08 13:25:09 -0600

    Interface

---
+++ BMWClaseC.java
@@ -0,0 +1,26 @@
+/**
+ * Crear una interface para implementar en metodos posteriormente
+ * @author José Rivera
+ * @author Oscar
+ */
+
+ public interface BMWClaseC{
+    /**
+     * 
+     * @return
+     */
+    public float mostrarTemp();
+    public void mostrarMenu();
+    public void limpiarPantalla();
+    public float pedirTemperatura(float temp);
+    public String mostrarEstadoVentilacion();
+    public String pedirNivelVentilacion(String nivel);
+    public String pedirZonaVentilacion(String nivel);
+    public String mostrarHumedad();
+    public void pedirNivelHumedad(String nivel);
+    public String mostrarEstado();
+    public String mostrarError();
+    public String mostrarAviso(String aviso);
+    public int obtenerOpcionMenu();
+    public boolean mostrarDesempañador();
+}
