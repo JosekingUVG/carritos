a87b8a5dd1f96a6ad165db9e44ed15c624b85872
Oscar Garcia <oegz03@gmail.com>
2024-11-11 13:02:50 -0600

    Creación del menú.

--- Principal.java
+++ Principal.java
@@ -2,14 +2,15 @@
 public class Principal{
     public static void main(String[] args) {
         BMWClaseC carro = new Carrito();
-        boolean encender = false;
+        boolean encender = true;
         
         System.out.println("SISTEMA DE CLIMATIZACIÓN");
         System.out.println("iniciar programa");
         while (encender==true) {
+            carro.mostrarMenu();
             int opcion = carro.obtenerOpcionMenu();
 
+        }
+        System.out.println("Gracias por utilizar este programa...");
     }
-    System.out.println("Gracias por utilizar este programa...");
-}
 }
